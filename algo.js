42f5666e42af230e5791a403d5509b3677ec5638
Mathieu <berthetmathieu@free.fr>
2023-05-22 15:56:57 +0200

    tri insertion et selection

--- algo.js
+++ algo.js
@@ -25,19 +25,54 @@ console.log("Tableau non trié : ", tabToSort);
 
         }
     }
+    console.log("Tableau trié : ", tabToSort);
 }
-console.log("Tableau trié : ", tabToSort);*/
+console.log("Tableau trié fini : ", tabToSort);*/
 
 //Tri Insertion
+let j = 0;
 
+/*for (let i = 0; i < tabToSort.length; i++)
+{
+    stockElement = tabToSort[i];
+    j = i;
+    while(j > 0 && tabToSort[j - 1] > stockElement)
+    {
+        tabToSort[j] = tabToSort[j - 1];
+        j = j -1;
+    }
 
+    tabToSort[j] = stockElement;
+    console.log("Tableau trié, tri insertion : ", tabToSort);
+}
+console.log("Tableau trié fini, tri insertion : ", tabToSort);*/
 
 //Tri Selection
+let longueur = tabToSort.length;
+for (let i = 0; i < longueur - 2; i++)
+{
+    let min = i;
+    for(let j = i + 1; j <longueur; j++)
+    {
+        if(tabToSort[j] < tabToSort[min])
+        {
+            min = j;
+        }
+    }
+    if(min !== i)
+    {
+        stockElement = tabToSort[min];
+        tabToSort[min] = tabToSort[i];
+        tabToSort[i] = stockElement;
+    }
+    console.log("Tableau trié, tri selection : ", tabToSort);
+}
+console.log("Tableau trié fini, tri selection : ", tabToSort);
 
 
 
 //Tri a bulles
 
-
+//console.log("Tableau trié : ", tabToSort);
 
 //Tri de Shell
