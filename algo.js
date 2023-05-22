494d4bd5574e051305552172e155962a60d17ffa
Mathieu <berthetmathieu@free.fr>
2023-05-22 13:56:49 +0200

    init exo algo

---
+++ algo.js
@@ -0,0 +1,43 @@
+console.log("Coucou");
+
+
+let tabToSort = [3,9,7,1,6,2,8,4,5];
+let stockElement = 0;
+
+console.log("Tableau non trié : ", tabToSort);
+//Mon algo
+/*for(let i = 0; i < tabToSort.length; i++)
+{
+    for(let j = i + 1; j < tabToSort.length; j++)
+    {
+        if(tabToSort[j] > tabToSort[i])
+        {
+
+        }
+        else if(tabToSort[j] < tabToSort[i])
+        {
+            stockElement = tabToSort[j];
+            tabToSort[j] = tabToSort[i];
+            tabToSort[i] = stockElement;
+        }
+        else
+        {
+
+        }
+    }
+}
+console.log("Tableau trié : ", tabToSort);*/
+
+//Tri Insertion
+
+
+
+//Tri Selection
+
+
+
+//Tri a bulles
+
+
+
+//Tri de Shell
