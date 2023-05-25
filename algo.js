f0857db2bd62af1055d0088b9cbc634551368d39
MB <berthetmathieu@free.fr>
2023-05-25 13:37:27 +0200

    Add Quick sort and work on Merge sort

--- algo.js
+++ algo.js
@@ -7,6 +7,9 @@ let tabToSortBulle = [3,9,7,1,6,2,8,4,5];
 let tabToSortBulleStop = [3,9,7,1,6,2,8,4,5];
 let tabToSortShell = [3,9,7,1,6,2,8,4,5];
 let tabToSortPerso = [3,9,7,1,6,2,8,4,5];
+let tabToSortQuick = [3,9,7,1,6,2,8,4,5];
+let tabToSortMerge = [3,9,7,1,6,2,8,4,5];
+let tabToSortHeap = [3,9,7,1,6,2,8,4,5];
 let stockElement = 0;
 
 //Mon algo
@@ -153,12 +156,98 @@ function ShellSort(tab) {
     console.log("Tableau trié fini, tri par Shell : ", tab);
 }
 
+
+function partage(tab, first, last, pivot)
+{
+    console.log("Tableau non trié, tri RAPIDE 2 : ", tab);
+    stockElement = tab[pivot];
+    tab[pivot] = tab[last];
+    tab[last] = stockElement;
+
+    j = first;
+    for(let i = first; i < last; i++)
+    {
+        if(tab[i] < tab[last])
+        {
+            stockElement = tab[i];
+            tab[i] = tab[j];
+            tab[j] = stockElement;
+            j++;
+            console.log("Tableau trié, tri RAPIDE 1 : ", tab);
+        }
+    }
+    stockElement = tab[last];
+    tab[last] = tab[j];
+    tab[j] = stockElement;
+    console.log("Tableau  trié, tri RAPIDE 2 : ", tab);
+    return j;
+
+}
+function QuickSort(tab, firstElem, lastElem)
+{
+    console.log("Tableau non trié, tri RAPIDE : ", tab);
+    if(firstElem < lastElem)
+    {
+        let pivot = lastElem;
+        pivot = partage(tab, firstElem, lastElem, pivot);
+        QuickSort(tab, firstElem, pivot - 1);
+        QuickSort(tab, pivot + 1, lastElem);
+    }
+
+    console.log("Tableau trié fini, tri rapide : ", tab);
+}
+
+function MergeSort(tab)
+{
+    console.log("Tableau non trié, tri fusion : ", tab);
+    if(tab.length == 1)
+    {
+        console.log("Tableau trié fini, tri fusion : ", tab);
+        return tab;
+    }
+    else
+    {
+        fusion(MergeSort(tab[0], ...[tab.length/2]), MergeSort(tab[tab.length/2 + 1], ...[tab.length]));
+    }
+}
+
+function fusion(tabGauche, tabDroite)
+{
+    if(tabGauche === null)
+    {
+        return tabDroite;
+    }
+    if(tabDroite === null)
+    {
+        return tabGauche;
+    }
+    if(tabGauche[0] < tabDroite [0])
+    {
+        return tabGauche[0].concat(fusion(tabGauche[1], ...[tabGauche.length], tabDroite));
+        console.log("Tableau trié, tri FUSION 1 : ", tabGauche);
+    }
+    else
+    {
+        return tabDroite[0].concat(fusion(tabGauche, tabDroite[1], ...[tabDroite.length]));
+        console.log("Tableau trié, tri FUSION 2 : ", tabDroite);
+    }
+}
+
 //myAlgo(tabToSortPerso);
 //InsertionSort(tabToSortInsertion, 1, 0);
 //SelectionSort(tabToSortSelection);
 //BubbleSortWithoutStop(tabToSortBulle);
 //BubbleSortWithStop(tabToSortBulleStop);
 //ShellSort(tabToSortShell);
+//QuickSort(tabToSortQuick, 0, tabToSortQuick.length -1);
+MergeSort(tabToSortMerge);
+
+
+
+
+
+
+
 
 ////////////////////////////////////////////////////// RECURSIVITE //////////////////////////////////////////////////////////////
 function factorielle(n)
@@ -255,12 +344,12 @@ function pgcd(a,b)
 }
 
 
-console.log("factoriel : ", factorielle(5));
+/*console.log("factoriel : ", factorielle(5));
 
 console.log("fibo : ", fibo(12));
 
 
 syracuse(7);
 
-console.log("pgcd : ", pgcd(72,21));
-console.log("pgcd : ", pgcd(12,4));
+console.log("pgcd : ", pgcd(21,72));
+console.log("pgcd : ", pgcd(12,4));*/
